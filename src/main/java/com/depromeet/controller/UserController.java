c0d2fcfe1d5441c3a4723da9c640642f9d53af36
BeINone <sbeinone@gmail.com>
2018-04-07 17:36:02 +0900

    회원가입 기능 추가

--- src/main/java/com/depromeet/controller/UserController.java
+++ src/main/java/com/depromeet/controller/UserController.java
@@ -1,16 +1,22 @@
 package com.depromeet.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.depromeet.models.dto.RegisterRequest;
 import com.depromeet.models.dto.UserResponse;
 import com.depromeet.service.UserService;
 
-@Controller
+@RestController
 @RequestMapping(path="/")
 public class UserController {
 	@Autowired
@@ -19,5 +25,11 @@ public class UserController {
 	@GetMapping(path="/user")
 	public @ResponseBody List<UserResponse> getAllUsers() {
 		return userService.getAllUser();
+	}
+	
+	@PostMapping(path="/register")
+	@ResponseStatus(HttpStatus.CREATED)
+	public @ResponseBody void register(@RequestBody RegisterRequest request) {
+		userService.addUser(request);
 	}
 }
--- src/main/java/com/depromeet/service/UserService.java
+++ src/main/java/com/depromeet/service/UserService.java
@@ -2,26 +2,35 @@ package com.depromeet.service;
 
 import java.util.LinkedList;
 import java.util.List;
+
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.depromeet.models.dto.RegisterRequest;
 import com.depromeet.models.dto.UserResponse;
 import com.depromeet.models.entity.User;
 import com.depromeet.repository.UserRepository;
 
 @Service
+@Transactional
 public class UserService {
+	
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
 
-	@Transactional
 	public List<UserResponse> getAllUser() {
 		List<User> user = userRepository.findAll();
 		List<UserResponse> list = new LinkedList<>();
 		for(User u : user) {
-			list.add(new UserResponse().from(u));
+			list.add(UserResponse.from(u));
 		}
 		
 		return list;
+	}
+	
+	public User addUser(RegisterRequest request) {
+		return userRepository.save(User.from(request));
 	}
 }
