38a2a3f6d9b93c22ed30d277e9f0df2b32f124ef
Jinhyeon Park <wlsgussla123@gmail.com>
2018-04-08 21:28:42 +0900

    이미지 저장

---
+++ src/main/java/com/depromeet/controller/ImageUploadController.java
@@ -0,0 +1,49 @@
+package com.depromeet.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@RestController
+@RequestMapping("/image")
+public class ImageUploadController {
+	private static String UPLOADED_FOLDER = "C://dev//DPM2018//mini-team3-api//mini-team3-api//images/";
+	
+
+    @PostMapping("/upload") // //new annotation since 4.3
+	@ResponseStatus(HttpStatus.OK)
+    public void singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return;
+        }
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            System.out.println(path);
+            Files.write(path, bytes);
+            System.out.println(file.getOriginalFilename());
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return;
+    }
+	
+}
